6257de0113159d4a9aa830d5ceb52720611a3e52
roivasxxx <tiragoshi@gmail.com>
2026-04-15 17:24:04 +0200

    change

--- tests/add_researchers.spec.ts
+++ tests/add_researchers.spec.ts
@@ -24,7 +24,7 @@ test.beforeEach(async ({ page }) => {
   await page.locator('[name="j_id_8:password"]').fill(process.env.PASSWORD);
   await page.locator('[name="j_id_8:login"]').click();
 
-  console.log("change")
+  console.log("change1")
 
   await expect(page).toHaveURL(/home/);
 
